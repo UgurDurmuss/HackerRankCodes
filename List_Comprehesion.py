c357780c112fcc6cea580455c4d61623164f83f8
uğur <ugurdu65@gmail.com>
2025-06-13 01:59:11 +0300

    7.Hacker Rank Kodu

---
+++ List_Comprehesion.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+x=[[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1)
+if i+j+k!=n]#i,j,k üzerinden birkaç tane for açtık taska göre değerlerimiz sıfırdan
+#başlıyor zaten bunun için range kullanık ve girilen x y z değerlerine göre i,j,k leri
+#list comprehension ile belirledik,i j k değerlerinin toplamı sınır olan n e eşit olan list'leri çıkardık
+
+print(x)#herhangi bir değere eşitleyip yazdık list comprehesionu
