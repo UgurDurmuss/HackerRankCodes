2680fdb998b85fee2c3ddfbc5951d75129360d16
Uğur Durmus <ugurdu65@gmail.com>
2025-06-13 00:13:37 +0300

    1.Hacker Rank kodu

---
+++ Elseİf.py
@@ -0,0 +1,15 @@
+n = int(input())  # Kodlar python 3 ile yazıldı
+if n % 2:
+    print("Weird")
+elif 2 <= n <= 5:
+    print("Not Weird")
+elif 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
+
+
+
+
+
+
